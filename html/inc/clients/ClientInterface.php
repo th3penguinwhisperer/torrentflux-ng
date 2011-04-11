ae656c5034d779907b38ab69b11b81237fe0cb34
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-04-11 23:47:36 +0200

    Removed getActions from the ClientInterface class as it is specific to TransferInterface objects

--- html/inc/clients/ClientInterface.php
+++ html/inc/clients/ClientInterface.php
@@ -4,8 +4,6 @@ interface ClientInterface
 {
 	function getCapabilities();
 	
-	function getActions($transferhash);
-	
 	function executeAction($transfer, $action);
 	
 	function getTransferList($uid);
