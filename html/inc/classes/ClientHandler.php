9456d626b149fe78ec763f0595f2c700256d07e1
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-05-03 21:31:50 +0200

    Forgot to add ClientHandler. This returns an instance of the client that is managing the transfer

---
+++ html/inc/classes/ClientHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('inc/singleton/Configuration.php');
+
+class ClientHandler
+{	
+	/**
+	 * get ClientHandler-instance
+	 *
+	 * @param $client client-type
+	 * @return ClientHandler
+	 */
+	static function getInstance($client = "") {
+		
+		// create and return object-instance
+		switch ($client) {
+		case "transmission-daemon":
+			require_once('inc/clients/transmission-daemon/TransmissionDaemonClient.php');
+			$handler = TransmissionDaemonClient::getInstance();
+			break;
+		default:
+			$cfg = Configuration::get_instance()->get_cfg();
+			$handler = ClientHandler::getInstance($cfg["btclient"]);
+		}
+		return $handler;
+		
+	}
+	
+}
+
+?>
