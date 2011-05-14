61cca55c89d30686e6c555bb14560361092290a7
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-05-14 22:00:10 +0200

    Made javascript functions more generic

--- html/js/transferlist.js
+++ html/js/transferlist.js
@@ -1,13 +1,23 @@
-function gettransferlist() {
-    //$(\'#transferlist\').html(\'Loading..\');
-    $.get('index.php?page=transferlist', function(data) {
-        $('#transferlist').html(data);
+function loadcontent(divname, url, loadingmsg) {
+	if (loadingmsg != null)
+		$('#' + divname).html(loadingmsg);
+    $.get(url, function(data) {
+        $('#' + divname).html(data);
         //window.setTimeout(update, 10000);
     });
+}
+
+function gettransferlist(divname) {
+    loadcontent(divname, 'index.php?page=transferlist');
 };
 
-function reloadtransferlist() {
+function gettransfersources(divname, parameters) {
+	if (parameters == null) parameters = "";
+    loadcontent(divname, 'index.php?page=transfersources' + parameters, "Loading transfer source plugins ...");
+};
+
+function reloadtransferlist(divname) {
     var refreshId = setInterval(
-        function() { gettransferlist(); },
+        function() { gettransferlist(divname); },
         9000);
 };
