17443c212e8d2f8e57c3676393fb46a05d45c4ee
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-05-14 23:56:42 +0200

    Forgot to commit new js function headlessaction

--- html/js/transferlist.js
+++ html/js/transferlist.js
@@ -13,11 +13,26 @@ function gettransferlist(divname) {
 
 function gettransfersources(divname, parameters) {
 	if (parameters == null) parameters = "";
-    loadcontent(divname, 'index.php?page=transfersources' + parameters, "Loading transfer source plugin ...");
+    loadcontent(divname, 'index.php?page=transfersources' + parameters, "Loading transfer source plugin ...<br><img src=images/ajax-loader.gif>");
 };
 
 function reloadtransferlist(divname) {
     var refreshId = setInterval(
         function() { gettransferlist(divname); },
         9000);
+};
+
+function headlessaction(action, reload, message) {
+	$.get(action, function(data) {
+		$('#status_message').html(message);
+		        $("#status_message").show();
+		        var refreshId = setTimeout(
+		            function() {
+		                $("#status_message").val("");
+		                $("#status_message").hide();
+		            }, 
+		            5000
+		        );
+    });
+	if (reload == true) reloadtransferlist("transferlist");
 };
