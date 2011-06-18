cd6d7b5f5b6cea62e3c7511c3c280bb8a6f3a952
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-06-18 20:45:20 +0200

    Disable/enable plugins feature

--- html/configure.php
+++ html/configure.php
@@ -9,14 +9,38 @@ else $action = $_REQUEST['action'];
 
 if( $action == "get" ) {
   $ph = new PluginHandler();
+  
+  print("==== Enabled plugins ===<br>");
   $pluginNames = $ph->getEnabledPlugins();
   foreach( $pluginNames as $plugin ) {
-    print("$plugin[0]<br>");
+    getConfigurationUi($ph, $plugin);
+    print("<hr>");
+  }
+
+  print("=== Disabled plugins ===<br>");
+  $pluginNames = $ph->getDisabledPlugins();
+  foreach( $pluginNames as $plugin ) {
+    getConfigurationUi($ph, $plugin);
+    print("<hr>");
+  }
+}
+
+function getConfigurationUi($ph, $plugin)
+{
+    print("$plugin[1] ");
+    
+    if ($plugin[4] == 1) // TODO: if enabled
+      print("<a href=\"configure.php?action=disable&plugin=$plugin[0]\"><img></a><br>");
+    else
+      print("<a href=\"configure.php?action=enable&plugin=$plugin[0]\"><img></a><br>");
+
     if( $plugin[0] == "rss-transfers" ) { // TODO: this is only temporarily until other plugins are implementing from PluginInterface as well
       $pi = $ph->getPlugin($plugin[0]);
-      $pi->getConfiguration();
+      if ( is_object($pi) ) {
+        $pi->getConfiguration();
+      }
     }
-  }
+
 }
 
 if ( $action == "set" ) {
@@ -26,8 +50,25 @@ if ( $action == "set" ) {
   $ph = new PluginHandler();
   $pi = $ph->getPlugin($pluginname);
   $pi->setConfiguration($_REQUEST);
-
-  header('Location: configure.php');
 }
+
+if ( $action == "enable" ) {
+  $pluginname = $_REQUEST['plugin'];
+
+  $ph = new PluginHandler();
+  $ph->enablePlugin($_REQUEST['plugin']);
+}
+
+if ( $action == "disable" ) {
+  $pluginname = $_REQUEST['plugin'];
+
+  $ph = new PluginHandler();
+  $ph->disablePlugin($_REQUEST['plugin']);
+
+}
+
+if ( $action != "get" )
+  header('Location: configure.php');
+
 
 ?>
