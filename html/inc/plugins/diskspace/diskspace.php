61d47813a9e876abf22fec3fb8d8b86be6c2eaa7
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-07-31 14:37:00 +0200

    Diskspace plugin now shows used diskspace in GB instead of free space in GB

--- html/inc/plugins/diskspace/diskspace.php
+++ html/inc/plugins/diskspace/diskspace.php
@@ -32,7 +32,9 @@ class DiskspaceInfo implements PluginInterface
 		$diskspaceusage = getDriveSpace( $cfg['download_path'] );
 		if ( $diskspaceusage > $cfg['diskusagewarninglevel'] ) $diskspacecolor = "#ff0000";
 		else $diskspacecolor = '#33cc33';
-		$output = $diskspaceusage . '% disk usage (' . formatBytesTokBMBGBTB( disk_free_space($cfg['download_path']) ) . "/" . formatBytesTokBMBGBTB( disk_total_space($cfg['download_path']) ) . ")";
+		$diskfreespace = disk_free_space($cfg['download_path']);
+		$disktotalspace = disk_total_space($cfg['download_path']);
+		$output = $diskspaceusage . '% disk usage (' . formatBytesTokBMBGBTB( $disktotalspace - $diskfreespace ) . "/" . formatBytesTokBMBGBTB( $disktotalspace ) . ")";
 		$output .= '<script type="text/javascript" src="js/diskspace.js"></script>';
 		$output .= '<script type="text/javascript">drawProgressBar(\'' . $diskspacecolor . '\', 300, ' . $diskspaceusage . ');</script>';
 		$output .= '<link rel="stylesheet" type="text/css" href="css/diskspace.css" />';
