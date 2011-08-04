65d1a563ca21d28e6a80a53953504904f9ec3d80
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2011-08-04 22:17:23 +0200

    Initial implementation of transferlist in RedRound theme

--- html/inc/page/index.php
+++ html/inc/page/index.php
@@ -24,8 +24,8 @@
 require_once("inc/generalfunctions.php");
 require_once('inc/singleton/Configuration.php');
 require_once('inc/plugins/PluginHandler.php');
-
-
+require_once('inc/lib/vlib/vlibTemplate.php');
+require_once('inc/functions.core.tmpl.php');
 
 function printHtml() {
   $cfg = Configuration::get_instance()->get_cfg();
@@ -68,6 +68,85 @@ pp = new Popup;
     print("<a href=configure.php>Configure</a>");
 }
 
-printHtml();
+
+
+$cfg = Configuration::get_instance()->get_cfg();
+if (isset($_REQUEST['ajax_update'])) {
+        $isAjaxUpdate = true;
+        $ajaxUpdateParams = tfb_getRequestVar('ajax_update');
+        // init template-instance
+        tmplInitializeInstance($cfg["theme"], "inc.transferList.tmpl");
+} else {
+        $isAjaxUpdate = false;
+        // init template-instance
+        tmplInitializeInstance($cfg["theme"], "page.index.tmpl");
+}
+
+// language
+/*
+$tmpl->setvar('_STATUS', $cfg['_STATUS']);
+$tmpl->setvar('_ESTIMATEDTIME', $cfg['_ESTIMATEDTIME']);
+$tmpl->setvar('_RUNTRANSFER', $cfg['_RUNTRANSFER']);
+$tmpl->setvar('_STOPTRANSFER', $cfg['_STOPTRANSFER']);
+$tmpl->setvar('_DELQUEUE', $cfg['_DELQUEUE']);
+$tmpl->setvar('_SEEDTRANSFER', $cfg['_SEEDTRANSFER']);
+$tmpl->setvar('_DELETE', $cfg['_DELETE']);
+$tmpl->setvar('_WARNING', $cfg['_WARNING']);
+$tmpl->setvar('_NOTOWNER', $cfg['_NOTOWNER']);
+$tmpl->setvar('_STOPPING', $cfg['_STOPPING']);
+$tmpl->setvar('_TRANSFERFILE', $cfg['_TRANSFERFILE']);
+$tmpl->setvar('_ADMIN', $cfg['_ADMIN']);
+$tmpl->setvar('_USER', $cfg['_USER']);
+$tmpl->setvar('_USERS', $cfg['_USERS']);
+
+// username
+$tmpl->setvar('user', $cfg["user"]);
+*/
+
+// language
+$tmpl->setvar('_STATUS', "Status");
+$tmpl->setvar('_ESTIMATEDTIME', "Est.");
+$tmpl->setvar('_RUNTRANSFER', "Run");
+$tmpl->setvar('_STOPTRANSFER', "Stop");
+$tmpl->setvar('_DELQUEUE', "Delete from queue");
+$tmpl->setvar('_SEEDTRANSFER', "Seed");
+$tmpl->setvar('_DELETE', "Delete");
+$tmpl->setvar('_WARNING', "Warning");
+$tmpl->setvar('_NOTOWNER', "Not owner");
+$tmpl->setvar('_STOPPING', "Stopping");
+$tmpl->setvar('_TRANSFERFILE', "Transferfile");
+$tmpl->setvar('_ADMIN', "Administrator");
+$tmpl->setvar('_USER', "User");
+$tmpl->setvar('_USERS', "Users");
+
+// username
+$tmpl->setvar('user', $cfg["user"]);
+
+
+$arUserTorrent = array();
+$arListTorrent = array(); // TODO where is this array needed for?
+if ($cfg["transmission_rpc_enable"]) {
+	require_once('inc/clients/transmission-daemon/TransmissionDaemonClient.php');
+	$td = new TransmissionDaemonClient();
+	$transfers = $td->getTransferList($_SESSION['uid']);
+	
+	foreach($transfers as $transfer)
+		array_push($arUserTorrent, $transfer->getTransferListItem());
+}
+
+$tmpl->setloop('arUserTorrent', $arUserTorrent);
+$tmpl->setloop('arListTorrent', $arListTorrent);
+
+if (sizeof($arUserTorrent) > 0) 
+        $tmpl->setvar('are_user_transfer', 1);
+$boolCond = true;
+if ($cfg['enable_restrictivetview'] == 1)
+        $boolCond = $cfg['isAdmin']; // TODO Err... what does this do? :)
+$tmpl->setvar('are_transfer', (($boolCond) && (sizeof($arListTorrent) > 0)) ? 1 : 0);
+
+
+$tmpl->pparse();
+
+//printHtml();
 
 ?>
