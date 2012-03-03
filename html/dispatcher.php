80ca4ff28e6664ab93de2b05253113dd8b6cfe53
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2012-03-03 20:31:07 +0100

    Upload of metafile is now in an iframe so page doesn't get reloaded

--- html/dispatcher.php
+++ html/dispatcher.php
@@ -24,7 +24,16 @@ if ( isset($action) ) {
 	if ($action == "deletewithdata")	$client->deletewithdata($transfer);
 	if ($action == "add") { 			$client->add($url, ( isset($_REQUEST['publictorrent']) && $_REQUEST['publictorrent'] == 'on' ?  getDownloadPath(true): getDownloadPath(false) ), ($subaction == "add" ? true : false)); exit(); }
 	if ($action == "transfertabs") {	$client->gettabs(); exit(); }
-	if ($action == "metafileupload") {	handleFileUpload($_FILES, $client, ( isset($_REQUEST['publictorrent']) && $_REQUEST['publictorrent'] == 'on' ?  getDownloadPath(true): getDownloadPath(false) ), ($subaction == "add" ? true : false)); }
+	if ($action == "metafileupload") {
+		if ( sizeof($_FILES) > 0 ) { // if a file is actually uploaded
+			handleFileUpload($_FILES, $client, ( isset($_REQUEST['publictorrent']) && $_REQUEST['publictorrent'] == 'on' ?  getDownloadPath(true): getDownloadPath(false) ), ($subaction == "add" ? true : false));
+			print("File was handled");
+		}
+			require_once('inc/plugins/PluginHandler.php');
+			$ph = new PluginHandler();
+			$plugin = $ph->getPlugin("metafileupload");
+			$plugin->printForm();
+	}
 }
 
 //header('Location: index.php'); // TODO: probably can be removed as actions are not shown
--- html/inc/plugins/uploadmetafile/transfersource.fileupload.php
+++ html/inc/plugins/uploadmetafile/transfersource.fileupload.php
@@ -6,14 +6,25 @@ class TransfersourceFileupload
 {
 
 	static function show() {
+		self::printIframe();
+	}
+
+	/* TODO: check how this issue can be resolved in a more generic way (the upload file form issue) */
+	static function printIframe() {
+		print("
+<iframe width=100% frameborder=0 src=\"dispatcher.php?action=metafileupload\">
+");
+	}
+	
+	static function printForm() {
 		print("
 <form name=uploadmetafile method=post action=dispatcher.php enctype=\"multipart/form-data\">Metafile upload
 	<input type=file name=metafile>
 	<br><input type=checkbox id=publictorrent name=publictorrent checked=checked> Public torrent
 ");
-	getClientSelection();
-	getActionSelection();
-	print("<input type=submit class=\"upload_file_button\">
+		getClientSelection();
+		getActionSelection();
+		print("<input type=submit class=\"upload_file_button\">
 	<input type=hidden name=action value=metafileupload>
 </form>");
 	}
