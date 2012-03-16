006412a4a5713893274279b051281e5e78bb718d
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2012-03-16 02:25:30 +0100

    Transfers can be added from a search result now

--- html/inc/plugins/searchengines/PirateBayEngine.php
+++ html/inc/plugins/searchengines/PirateBayEngine.php
@@ -550,8 +550,8 @@ class pBay
 		}
 
 		$output = "<tr>\n";
-		$output .= "	<td width=\"16\" bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".$this->torrentFile."\"><img src=\"".getImagesPath()."download_owner.gif\" width=\"16\" height=\"16\" title=\"".$this->torrentName."\" border=0></a></td>\n";
-		$output .= "	<td bgcolor=\"".$bg."\"><a href=\"dispatcher.php?action=urlUpload&type=torrent&url=".$this->torrentFile."\" title=\"".$this->torrentName."\">".$this->torrentDisplayName."</a></td>\n";
+		$output .= "	<td width=\"16\" bgcolor=\"".$bg."\"><a href=\"javascript:addTransfer(escape('".$this->torrentFile."'));\"><img src=\"".getImagesPath()."download_owner.gif\" width=\"16\" height=\"16\" title=\"".$this->torrentName."\" border=0></a></td>\n";
+		$output .= "	<td bgcolor=\"".$bg."\"><a href=\"javascript:addTransfer(escape('".$this->torrentFile."'));\" title=\"".$this->torrentName."\">".$this->torrentDisplayName."</a></td>\n"; // USE ESCAPE FUNCTION OTHERWHISE TITLE IS NOT SHOWN BEFORE THE TORRENT IS DOWNLOADED, ONLY THE ID IS SHOWN THEN
 
 		if (strlen($this->MainCategory) > 1){
 			if (strlen($this->SubCategory) > 1){
--- html/inc/plugins/searchengines/searchengines.php
+++ html/inc/plugins/searchengines/searchengines.php
@@ -15,67 +15,93 @@ class SearchEngines implements PluginInterface
 	{
 		if ( ! isset($_REQUEST['action']) ) $_REQUEST['action'] = '';
 		if ( $_REQUEST['action'] != 'passplugindata' ) {
-			print("
-<form name=searchengine action=\"\">Torrent Search Query:
-<script type=\"text/javascript\">
+			print('
+<form name=searchengine action="">Torrent Search Query:
+<script type="text/javascript">
 
 $(function() {
-  $(\".search_button\").click( function() {
+  $(".search_button").click( function() {
     // Copy this part as much as necessary
-    var query = escape( $(\"input#query\").val() );
-    if (query == \"\") {
-      //$(\"label#name_error\").show();
-      $(\"#status_message\").show();
-      $(\"#status_message\").html(\"The search field is empty\");
-      $(\"input#query\").focus();
+    var query = escape( $("input#query").val() );
+    if (query == "") {
+      //$("label#name_error").show();
+      $("#status_message").show();
+      $("#status_message").html("The search field is empty");
+      $("input#query").focus();
       return false;
     }
 
     // get other values
-    var client = $(\"#client\").val();
-    var action = $(\"#action\").val();
-    var subaction = $(\"#subaction\").val();
-    var plugin = $(\"#plugin\").val();
-    if ( $('#publictorrent').is(':checked') ) {
-		var publictorrent = 'on';
+    var client = $("#client").val();
+    var action = $("#action").val();
+    var subaction = $("#subaction").val();
+    var plugin = $("#plugin").val();
+    if ( $("#publictorrent").is(":checked") ) {
+		var publictorrent = "on";
 	} else {
-		var publictorrent = 'off';
+		var publictorrent = "off";
 	}
 
     // validate and process form here
-    var dataString = 'query=' + query + '&client=' + client + '&action=' + action + '&subaction=' + subaction + '&plugin=' + plugin + '&publictorrent=' + publictorrent;
+    var dataString = "query=" + query + "&client=" + client + "&action=" + action + "&subaction=" + subaction + "&plugin=" + plugin + "&publictorrent=" + publictorrent;
 
     $.ajax({
-      type: \"POST\",
-      url: \"dispatcher.php\",
+      type: "POST",
+      url: "dispatcher.php",
       data: dataString,
       success: function(data) {
-	$(\"#query\").val(\"\");
-        $(\"#searchresult\").html(data);
-	    showstatusmessage(\"New transfer is added\");
-        reloadtransferlist();
+	$("#query").val("");
+        $("#searchresult").html(data);
       },
       error: function() {
-        showstatusmessage(\"Adding the transfer was not successful\");
+        showstatusmessage("Error while searching for torrents!");
       }
     });
     return false;
   });
 });
+
+	function addTransfer(url)
+	{
+	    // get other values
+	    var client = $("#client").val();
+	    var subaction = $("#subaction").val();
+	    if ( $("#publictorrent").is(":checked") ) {
+		var publictorrent = "on";
+	    } else {
+		var publictorrent = "off";
+	    }
+	
+	    // validate and process form here
+	    // TODO: all plugins jquery have to be edited/implemented when an option is added, which is error prone and might be fixed in a cleaner way
+	    var dataString = \'url=\' + url + \'&client=\' + client + \'&action=add\' + \'&subaction=\' + subaction + \'&publictorrent=\' + publictorrent;
+	
+	    $.ajax({
+	      type: "POST",
+	      url: "dispatcher.php",
+	      data: dataString,
+	      success: function() {
+		showstatusmessage("Transfer added");
+		reloadtransferlist();
+	      }
+	    });
+	}
+
+
 </script>
 
 	<input type=text name=query id=query>
 	<br><input type=checkbox id=publictorrent name=publictorrent checked=checked> Public torrent
-");
+');
 
 	getClientSelection();
 	getActionSelection();
-	print("<input type=submit class=\"search_button\">
+	print('<input type=submit class="search_button">
 	<input type=hidden name=action id=action value=passplugindata>
 	<input type=hidden name=plugin id=plugin value=searchengines>
 </form>
 <div id=searchresult name=searchresult />
-");
+');
 		} else {
 			require_once('inc/plugins/searchengines/torrentSearch.php');
 			print( getPage() ); // prints the string
