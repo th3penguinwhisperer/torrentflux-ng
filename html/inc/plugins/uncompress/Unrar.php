e3de4627da79b69bb030841ea8190de65e82be16
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2012-06-04 14:52:05 +0200

    Unrar and UncompressBaseClass now modified to use new Process class

--- html/inc/plugins/uncompress/Unrar.php
+++ html/inc/plugins/uncompress/Unrar.php
@@ -2,50 +2,51 @@
 
 require_once("inc/plugins/uncompress/UncompressBaseClass.php");
 require_once('inc/classes/singleton/Configuration.php');
+require_once('inc/classes/Process.php');
 
 class Unrar extends UncompressBaseClass
 {
 
 	function checkstatus()
 	{
-		if (file_exists($this->dir.$this->filename.".".Unrar::$logfile)) {
-			$lines = file($this->dir.$this->filename.".".Unrar::$logfile);
+		if ( $this->proc->haslogfile() ) {
+			$lines = file( $this->proc->getlogfilename() );
 			foreach($lines as $chkline) {
 				if (strpos($chkline, 'already exists. Overwrite it ?') !== FALSE){
-					$this->kill($this->pid);
+					$this->proc->kill();
 					echo 'File has already been extracted, please delete extracted file if re-extraction is necessary.';
 					break 2;
 				}
 				if (strpos($chkline, 'Cannot find volume') !== FALSE){
-					$this->kill($this->pid);
+					$this->proc->kill();
 					echo 'File has a missing volume and can not been extracted.';
 					break 2;
 				}
 				if (strpos($chkline, 'ERROR: Bad archive') !== FALSE){
-					$this->kill($this->pid);
+					$this->proc->kill();
 					echo 'File has a bad volume and cannot be extracted.';
 					break 2;
 				}
 				if (strpos($chkline, 'CRC failed') !== FALSE){
-					$this->kill($this->pid);
+					$this->proc->kill();
 					echo 'File extraction has failed with a CRC error and was not been extracted.';
 					break 2;
 				}
 				if (strpos($chkline, 'User break') !== FALSE){
 					echo 'The uncompress process has been killed by a user.';
-					$this->cleanup();
+					$this->cleanup(); // not the $proc cleanup! that is called in this class cleanup method
 					break 1;
 				}
 			}
-			if ($this->is_running($this->pid)) {
+			if ($this->proc->is_running()) {
 				// None of the above apply so extracting is still running. Filter out the percentage
 				$filecontent = implode("",$lines);
 				preg_match_all('/[0-9]{1,2}\%/', $filecontent, $res );
 				echo "Extract running: ".end(end($res));
 			}
 		}
-		if (file_exists($this->dir.$this->filename.".".Unrar::$logfile)) {
-			$lines = file($this->dir.$this->filename.".".Unrar::$logfile);
+		if ($this->proc->haslogfile()) {
+			$lines = file($this->proc->getlogfilename());
 			foreach($lines as $chkline) {
 				if (strpos($chkline, 'All OK') !== FALSE){
 					echo 'File has successfully been extracted!';
@@ -58,15 +59,13 @@ class Unrar extends UncompressBaseClass
 	{
 		$cfg = Configuration::get_instance()->get_cfg();
 		
-		$pid = $this->getpid();
-		if (file_exists($this->dir.$this->filename.".".Unrar::$logfile)) {
+		if ($this->proc->haslogfile()) {
 			$this->checkstatus();
 		} else {
 			$passcmdpart = ( $this->password == "" ? "" : "-p".tfb_shellencode($this->password) );
 			$Command = tfb_shellencode($cfg['rewrite_bin_unrar'])." x -o+ $passcmdpart ". tfb_shellencode($this->dir.$this->filename) . " " . tfb_shellencode($this->dir);
-			$pid = trim(shell_exec("nohup ".$Command." > " . tfb_shellencode($this->dir.$this->filename.".".Unrar::$logfile) . " 3>&1 & echo $!"));
-			echo 'Uncompressing started...<BR>PID is: ' . $pid . '<BR>';
-			$this->setpid($pid);
+			$this->proc->execute($Command);
+			echo 'Uncompressing started...<BR>PID is: ' . $this->proc->getpid() . '<BR>';
 			usleep(250000); // wait for 0.25 seconds
 			$this->checkstatus();
 		}
