a115dbe9c7908694e0f521cf9ac6bdab14d7e693
th3penguinwhisperer <th3penguinwhisperer@gmail.com>
2013-05-06 17:45:52 +0200

    Made list of clients dynamic Closes #34

--- html/inc/classes/ClientHandler.php
+++ html/inc/classes/ClientHandler.php
@@ -1,29 +1,63 @@
 <?php
 
 require_once('inc/classes/singleton/Configuration.php');
+require_once('inc/classes/singleton/db.php');
 
 class ClientHandler
 {	
+	static private $ch;
+
+	private $handle;
+	private $clients = array();
+
+	private function __construct() {
+		require_once('inc/classes/singleton/db.php');
+		$db = DB::get_db()->get_handle();
+
+		// get the clients that are saved in the database
+		$sql = "SELECT pluginname, plugininclude, pluginclass FROM tf_plugins WHERE plugintype='transferclient' AND pluginenabled='1'";
+		$recordset = $db->Execute($sql);
+		if ($db->ErrorNo() != 0) dbError($sql);
+		$clients = array();
+		while($clientinfo = $recordset->FetchRow()) {
+			require_once($clientinfo['plugininclude']);
+			$client = $clientinfo['pluginclass']::getInstance();
+			$clients[$clientinfo['pluginname']] = $client;
+		}
+		$this->clients = $clients;
+	}
+   
+	static function get_ch()
+	{
+		if ( ! isset(ClientHandler::$ch) )
+			ClientHandler::$ch = new ClientHandler();
+		return ClientHandler::$ch;
+	}
+ 
+	function get_handle()
+	{
+		return $this->handle;
+	}
+
+
 	/**
 	 * get ClientHandler-instance
 	 *
 	 * @param $client client-type
 	 * @return ClientHandler
 	 */
-	static function getInstance($client = "") {
-		
-		// create and return object-instance
-		switch ($client) {
-		case "transmission-daemon":
-			require_once('inc/clients/transmission-daemon/TransmissionDaemonClient.php');
-			$handler = TransmissionDaemonClient::getInstance();
-			break;
-		default:
-			$cfg = Configuration::get_instance()->get_cfg();
-			$handler = ClientHandler::getInstance($cfg["rewrite_btclient"]);
+	function getInstance($client = "") {
+		$cfg = Configuration::get_instance()->get_cfg();
+
+		if( isset($this->clients[$client]) ) {
+			return $this->clients[$client];
+		} elseif( isset($cfg["rewrite_btclient"]) ) {
+			return $this->getInstance($cfg["rewrite_btclient"]);
 		}
-		return $handler;
-		
+
+		AuditAction("CLIENTHANDLER", $cfg["constants"]["error"], "ClientHandler does not have an instance of client $client");
+
+		return false;
 	}
 	
 }
